d3d549ed52ffd7ac0856d88fee9a6bea6dacbeb1
jungkyu123 <jungkyu.l@hotmail.com>
2021-11-12 12:41:33 +0700

    cart-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // feature 1
 import React from 'react'
+import Cart from './components/Cart';
 import Filter from './components/Filter';
 import Products from './components/Products';
 import data from './data.json'
@@ -9,9 +10,35 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: "",
       sort: "",
     };
+  }
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({ 
+      cartItems: cartItems.filter(x => x._id !== product._id),
+    });
+    // with this command, we get rid of the current product that the user selected to remove
+    // cartItems.filter(x => x._id !== product._id);
+  }
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+
+    cartItems.forEach(item => {
+      if(item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if(!alreadyInCart) {
+      cartItems.push({...product, count: 1});
+    }
+    this.setState({cartItems});
   }
 
   sortProducts = (event) => {
@@ -63,10 +90,10 @@ class App extends React.Component {
                 sortProducts = {this.sortProducts}
                 filterProducts = {this.filterProducts}
               />
-              <Products products={this.state.products} />
+              <Products products={this.state.products} addToCart={this.addToCart} />
             </div>
             <div className="sidebar">
-              Cart Items
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />
             </div>
           </div>
         </main>
